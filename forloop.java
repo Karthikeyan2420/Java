f3af46b7ea034bad36822939cf187e68dc97b2a6
Karthikeyan2420 <84924959+Karthikeyan2420@users.noreply.github.com>
2024-01-19 19:42:28 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,30 @@
+package class1;
+
+import java.util.Scanner;
+
+public class c3 {
+	
+	
+	public static void main(String[] args) {
+		
+		//for(initial;condition;increament/decreament)
+		
+		int i=0;
+		for (i=0;i<5;i++) {
+			System.out.println("This is for loop "+i);
+		}
+		
+		
+		for (int j=0;j<5;j++) {
+			System.out.println("This is for loop "+j);
+		}
+		
+		
+		for (int x=5;x>0;x--) {
+			System.out.println("this is for decr loop"+x);
+		}
+		
+	}
+	
+
+}
